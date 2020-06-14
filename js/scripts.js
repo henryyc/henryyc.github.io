ab8d1070a66b50efa3df5b9208dd6348c04e0191
Henry Chen <hyc@umd.edu>
2020-06-14 17:08:34 -0400

    remove exp page

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,13 @@ $('#home-link').click(function() {
 	$('.about').fadeOut('fast', function() {
         document.getElementById('about-link').classList.remove('current');
 
-        // fade out experience
-        $('.experience').fadeOut('fast', function() {
-            document.getElementById('experience-link').classList.remove('current');
+        // fade in home
+        document.getElementById('home-link').classList.add('current');
+        $('.home').fadeIn('slow');
 
-            // fade in home
-            document.getElementById('home-link').classList.add('current');
-            $('.home').fadeIn('slow');
-
-						// un-darken background
-						document.getElementById('main').classList.remove('darken');
-						document.getElementById('main').classList.add('undarken');
-        });
+				// un-darken background
+				document.getElementById('main').classList.remove('darken');
+				document.getElementById('main').classList.add('undarken');
     });
 });
 
@@ -25,39 +20,13 @@ $('#about-link').click(function() {
 	$('.home').fadeOut('fast', function() {
         document.getElementById('home-link').classList.remove('current');
 
-        // fade out experience
-        $('.experience').fadeOut('fast', function() {
-            document.getElementById('experience-link').classList.remove('current');
+        // fade in about
+        document.getElementById('about-link').classList.add('current');
+        $('.about').fadeIn('slow');
 
-            // fade in about
-            document.getElementById('about-link').classList.add('current');
-            $('.about').fadeIn('slow');
-
-						// darken background
-						document.getElementById('main').classList.remove('undarken');
-						document.getElementById('main').classList.add('darken');
-        });
-    });
-});
-
-// go to experience page
-$('#experience-link').click(function() {
-    // fade out home
-	$('.home').fadeOut('fast', function() {
-        document.getElementById('home-link').classList.remove('current');
-
-        // fade out about
-        $('.about').fadeOut('fast', function() {
-            document.getElementById('about-link').classList.remove('current');
-
-            // fade in experience
-            document.getElementById('experience-link').classList.add('current');
-            $('.experience').fadeIn('slow');
-
-						// darken background
-						document.getElementById('main').classList.remove('undarken');
-						document.getElementById('main').classList.add('darken');
-        });
+				// darken background
+				document.getElementById('main').classList.remove('undarken');
+				document.getElementById('main').classList.add('darken');
     });
 });
 
@@ -71,37 +40,4 @@ $('.typedDes').typed({
     loop: false,
     startDelay: 250,
     backDelay: 1000
-});
-
-// modal max height - not tested just copied xd
-function setModalMaxHeight(element) {
-  this.$element     = $(element);
-  this.$content     = this.$element.find('.modal-content');
-  var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
-  var dialogMargin  = $(window).width() < 768 ? 20 : 60;
-  var contentHeight = $(window).height() - (dialogMargin + borderWidth);
-  var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
-  var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
-  var maxHeight     = contentHeight - (headerHeight + footerHeight);
-
-  this.$content.css({
-      'overflow': 'hidden'
-  });
-
-  this.$element
-    .find('.modal-body').css({
-      'max-height': maxHeight,
-      'overflow-y': 'auto'
-  });
-}
-
-$('.modal').on('show.bs.modal', function() {
-  $(this).show();
-  setModalMaxHeight(this);
-});
-
-$(window).resize(function() {
-  if ($('.modal.in').length != 0) {
-    setModalMaxHeight($('.modal.in'));
-  }
 });
